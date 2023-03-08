b53b7cedbd68a0303202e94353fbcd03abb9fedc
Shamil <dnumkfde2203@mail.ru>
2023-03-08 19:52:02 +0500

    добавил первый класс. Rational - поле рациональных чисел. Написаны необходимы методы, но в будущем нужно будет добавить некоторые методы

---
+++ programm/GroebnerLib/includes/Rational.h
@@ -0,0 +1,41 @@
+#ifndef PROGRAMM_RATIONAL_H
+#define PROGRAMM_RATIONAL_H
+
+#include "Utils.h"
+
+namespace gb::fields {
+    class Rational {
+
+    public:
+        Rational(int64_t = 0, int64_t = 1);
+
+        const int64_t &getNumerator() const noexcept;
+        const int64_t &getDenominator() const noexcept;
+
+        friend bool operator<(const Rational &, const Rational &) noexcept;
+        friend bool operator>(const Rational &, const Rational &) noexcept;
+        friend bool operator<=(const Rational &, const Rational &) noexcept;
+        friend bool operator>=(const Rational &, const Rational &) noexcept;
+        friend bool operator==(const Rational &, const Rational &) noexcept;
+        friend bool operator!=(const Rational &, const Rational &) noexcept;
+
+        Rational operator+() const noexcept;
+        Rational operator-() const noexcept;
+        Rational& operator+=(const Rational&) noexcept;
+        friend Rational operator+(const Rational&, const Rational&) noexcept;
+        Rational& operator-=(const Rational&) noexcept;
+        friend Rational operator-(const Rational&, const Rational&) noexcept;
+
+        Rational& operator*=(const Rational&) noexcept;
+        friend Rational operator*(const Rational&, const Rational&) noexcept;
+        Rational& operator/=(const Rational&);
+        friend Rational operator/(const Rational&, const Rational&);
+
+    private:
+        void reduce_();
+
+        int64_t numerator_;
+        int64_t denominator_;
+    };
+}
+#endif //PROGRAMM_RATIONAL_H
---
+++ programm/GroebnerLib/includes/Utils.h
@@ -0,0 +1,21 @@
+#ifndef PROGRAMM_UTILS_H
+#define PROGRAMM_UTILS_H
+
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdint>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <optional>
+#include <set>
+#include <stdexcept>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+#endif //PROGRAMM_UTILS_H
